9c2d7858125c276c3e70b67d91368fae6cec5728
Vasileios Kokotakis <vkokotakis@gmail.com>
2026-03-30 08:49:17 +0200

    Rename locations to Building 2-10, keep OTE Building name

--- src/data/evacuationRoutes.ts
+++ src/data/evacuationRoutes.ts
@@ -1,14 +1,14 @@
 export const LOCATIONS: Record<string, [number, number, number]> = {
-  'OTE Building':  [-40,   1,  25],
-  'Road Point 1':  [-43,   1,  18],
-  'Road Point 2':  [ -7,   1, -30],
-  'OAKA':          [ 15,   1, -15],
-  'two':           [-29.8, 1,  14],
-  'three':         [-15,   1,  20],
-  'four':          [  0,   1,  25],
-  'five':          [ 18.5, 1,  12],
-  'six':           [  1,   1,   2],
-  'seven':         [ 15,   1, -15],
+  'OTE Building': [-40,   1,  25],
+  'Building 2':   [-43,   1,  18],
+  'Building 3':   [ -7,   1, -30],
+  'Building 4':   [ 15,   1, -15],
+  'Building 5':   [-29.8, 1,  14],
+  'Building 6':   [-15,   1,  20],
+  'Building 7':   [  0,   1,  25],
+  'Building 8':   [ 18.5, 1,  12],
+  'Building 9':   [  1,   1,   2],
+  'Building 10':  [ 15,   1, -15],
 };
 
 export interface EvacuationRoute {
@@ -23,15 +23,15 @@ export const ROUTES: EvacuationRoute[] = [
   {
     id: 'ote-to-oaka-1',
     start: 'OTE Building',
-    safeSpot: 'OAKA',
-    waypoints: ['OTE Building', 'Road Point 1', 'Road Point 2', 'OAKA'],
+    safeSpot: 'Building 4',
+    waypoints: ['OTE Building', 'Building 2', 'Building 3', 'Building 4'],
     color: 'yellow',
   },
   {
     id: 'ote-to-oaka-2',
     start: 'OTE Building',
-    safeSpot: 'OAKA',
-    waypoints: ['OTE Building', 'two', 'three', 'four', 'five', 'six', 'seven'],
+    safeSpot: 'Building 10',
+    waypoints: ['OTE Building', 'Building 5', 'Building 6', 'Building 7', 'Building 8', 'Building 9', 'Building 10'],
     color: 'lime',
   },
 ];
